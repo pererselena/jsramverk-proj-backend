c9117d3b376838d3e3a95772ba0d00562be67a8e
Elena Perers <elena.perers@chasacademy.se>
2019-10-27 17:11:36 +0100

    Fixed buy and sell

--- routes/depot.js
+++ routes/depot.js
@@ -1,30 +1,41 @@
 'use strict';
 var depot = require('../src/depot');
 var User = require('../models/user');
+var Depot = require('../models/depot');
 
 var express = require('express');
 var router = express.Router();
+
+async function populateDepot(depot) {
+    var depot = await Depot.findById(depot).populate('items.product').exec()
+    return depot;
+}
 
 router.get('/:id', async function (req, res) {
     var user = await User.findById(req.params.id, function (err, docs) {
         return docs;
     });
+    var depot = await populateDepot(user.depot)
+    // var depot = await Depot.findById(user.depot, async function (err, docs) {
+    //     var test = await docs.populate('items.Product')
+    //     return test;
+    // });
+    console.log(depot)
     const data = {
         data: {
             title: "Depå",
-            items: user.depot.items,
+            items: depot.items,
             balance: user.balance,
             name: user.name
         }
-    };
-
+    };    
     res.json(data);
 });
 
 router.put('/buy',
-    (req, res) => depot.buy(res, req));
+    (req, res) => depot.buy(res, req.body));
 
-router.post('/sell', 
-    (req, res) => depot.sell(res, req));
+router.put('/sell', 
+    (req, res) => depot.sell(res, req.body));
 
 module.exports = router;
--- src/depot.js
+++ src/depot.js
@@ -1,20 +1,24 @@
 'use strict';
-var Depot = require('../models/depot');
 var User = require('../models/user');
 var Product = require('../models/product');
+var Depot = require('../models/depot')
 
 const depot = {
-    buy: async function (req) {
-        var depot = await Depot.findById(req.body.depot_id, function (err, docs) {
+    buy: async function (res, body) {
+        console.log(body);
+              
+        
+        var user = await User.findById(body.user_id, function (err, docs) {
             return docs;
         });
-        var user = await User.findById(req.body.user_id, function (err, docs) {
+        var depotId = user.depot;
+        var depot = await Depot.findById(depotId, function (err, docs) {
             return docs;
         });
-        var product = await Product.findById(req.body.product_id, function (err, docs) {
+        var product = await Product.findById(body.product_id, function (err, docs) {
             return docs;
         });
-        var amount = req.body.amount;
+        var amount = body.amount;
 
         // depot.items.forEach(element => {
         //     if (element.product._id === product._id) {
@@ -30,67 +34,69 @@ const depot = {
         var boughtProduct = {
             product: product,
             amount: amount,
-            boughtPrice: req.body.price
+            boughtPrice: body.price
         };
-
+        console.log(depot);
+        
         depot.items.push(boughtProduct);
         depot.save();
-        user.balance -= req.body.price * amount;
+        user.balance -= body.price * amount;
         user.save();
 
         return res.status(201).json({
             data: "bought"
         });
     },
-    sell: async function (req) {
-        var depot = await Depot.findById(req.body.depot_id, function (err, docs) {
+    sell: async function (res, body) {
+        console.log(body);
+        
+        var user = await User.findById(body.user_id, function (err, docs) {
             return docs;
         });
-        var user = await User.findById(req.body.user_id, function (err, docs) {
+        var depotId = user.depot;
+        var depot = await Depot.findById(depotId, function (err, docs) {
             return docs;
         });
-        var product = await Product.findById(req.body.product_id, function (err, docs) {
-            return docs;
-        });
-        var amount = req.body.amount;
+        var amount = body.amount;
         var msg = "";
         var status = 200;
         depot.items.forEach(element => {
-            if (element._id === req.body.order_id) {
+            if (element._id == body.product_id) {
                 if (element.amount > amount) {
                     element.amount -= amount;
-                    user.balance += req.body.price * amount;
+                    user.balance += body.price * amount;
                     depot.save();
                     user.save();
-                    return res.status(201).json({
-                        data: "sold"
-                    });
+                    status = 201;
+                    msg = "sold";
                 } else if (element.amount === amount) {
                     var index = depot.items.indexOf(element);
                     if (index > -1) {
                         depot.items.splice(index, 1);
-                        user.balance += req.body.price * amount;
+                        user.balance += body.price * amount;
+                        console.log(user.balance);
                         depot.save();
                         user.save();
-                        return res.status(201).json({
-                            data: "sold"
-                        });
+                        status = 201;
+                        msg = "sold";
                     } else {
-                        status = 500;
+                        status = 501;
                         msg = "Okänd orsak :("
                     }
                 } else {
-                    status = 500;
+                    status = 502;
                     msg = "För få produkter!";
                 }
             } else {
-                status = 500;
+                status = 503;
                 msg = "Du äger inte produkten!";
             }
         });
+        console.log("status: ", status);
+        
 
         return res.status(status).json({
-            errors: {
+            data: {
                 status: status,
                 detail: msg
             }
