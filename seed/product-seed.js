af61a11ff0c5e2d42460349eccf373be88fe72e3
Elena Perers <elena.perers@chasacademy.se>
2019-10-30 11:43:01 +0100

    Fixed cleanup for old collections in mongo

--- seed/product-seed.js
+++ seed/product-seed.js
@@ -9,59 +9,87 @@ Product.collection.drop();
 User.collection.drop()
 Depot.collection.drop();
 
-var products = [
-    new Product({
-        imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
-        title: 'BTHcoin',
-        description: 'Cryptocurrency',
-        rate: 1.002,
-        variance: 0.6,
-        startingPoint: 20
-    }),
-    new Product({
-        imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
-        title: 'JSgold',
-        description: 'Ordinary gold',
-        rate: 1.000,
-        variance: 0.6,
-        startingPoint: 40
-    }),
-    new Product({
-        imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
-        title: 'PHPsilver',
-        description: 'Ordinary silver',
-        rate: 1.000,
-        variance: 0.6,
-        startingPoint: 30
-    }),
-    new Product({
-        imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
-        title: 'Vlinux stock',
-        description: 'Stock',
-        rate: 1.000,
-        variance: 0.6,
-        startingPoint: 10
-    }),
-    new Product({
-        imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
-        title: 'Python stock',
-        description: 'Stock',
-        rate: 1.000,
-        variance: 0.6,
-        startingPoint: 10
-    })
-];
-
-var done = 0;
-for (var i = 0; i < products.length; i++) {
-    products[i].save(function (err, result) {
-        done++;
-        if (done === products.length) {
-            exit();
+async function cleanup() {
+    try {
+        await Product.collection.drop();
+    } catch (err) {
+        if (err.message !== 'ns not found') {
+            throw err;
         }
-    });
+    }
+
+    try {
+        await User.collection.drop();
+    } catch (err) {
+        if (err.message !== 'ns not found') {
+            throw err;
+        }
+    }
+
+    try {
+        await Depot.collection.drop();
+    } catch (err) {
+        if (err.message !== 'ns not found') {
+            throw err;
+        }
+    }
 }
 
-function exit() {
-    mongoose.disconnect();
-}
+cleanup().then(function () {
+    var products = [
+        new Product({
+            imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
+            title: 'BTHcoin',
+            description: 'Cryptocurrency',
+            rate: 1.002,
+            variance: 0.6,
+            startingPoint: 20
+        }),
+        new Product({
+            imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
+            title: 'JSgold',
+            description: 'Ordinary gold',
+            rate: 1.000,
+            variance: 0.6,
+            startingPoint: 40
+        }),
+        new Product({
+            imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
+            title: 'PHPsilver',
+            description: 'Ordinary silver',
+            rate: 1.000,
+            variance: 0.6,
+            startingPoint: 30
+        }),
+        new Product({
+            imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
+            title: 'Vlinux stock',
+            description: 'Stock',
+            rate: 1.000,
+            variance: 0.6,
+            startingPoint: 10
+        }),
+        new Product({
+            imagePath: 'https://borsvarlden.com/wp-content/uploads/2018/06/bvfa-s-cryptocurrency-0002-960x540.jpg',
+            title: 'Python stock',
+            description: 'Stock',
+            rate: 1.000,
+            variance: 0.6,
+            startingPoint: 10
+        })
+    ];
+
+    var done = 0;
+    for (var i = 0; i < products.length; i++) {
+        products[i].save(function (err, result) {
+            done++;
+            if (done === products.length) {
+                exit();
+            }
+        });
+    }
+
+    function exit() {
+        mongoose.disconnect();
+    }
+})
